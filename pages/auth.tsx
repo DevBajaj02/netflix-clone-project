556f06d73b485bbdc2abd261aec43884350b1985
Dev <devbajaj.bits@gmail.com>
2023-12-09 14:48:57 +0530

    login and redirect

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,11 @@
 import Input from "@/components/Input";
 import {useCallback, useState} from 'react';
 import axios from "axios";
+import { signIn } from 'next-auth/react';
+import { useRouter } from "next/router";
 const Auth = () =>{
+    const router = useRouter();
+
     const [email,setEmail]=useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -12,6 +16,21 @@ const Auth = () =>{
         setVariant((currentVariant)=> currentVariant==='login' ? 'register' : 'login');
     },[]);
 
+    const login = useCallback(async () =>{
+        try {
+            await signIn('credentials',{
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+            router.push('/');
+            console.log("loginn")
+        } catch (error) {
+            console.log(error);
+        }
+    },[email, password,router]);
+    
     const register = useCallback(async () => {
         try {
             console.log("hi");
@@ -21,13 +40,13 @@ const Auth = () =>{
                 name,
                 password
             });
-
+            login();
         } catch (error) {
-            console.log("hi11");
-
             console.log(error);
         }
-    }, [email, name, password]);
+    }, [email, name, password, login]);
+
+    
     return(
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -64,7 +83,7 @@ const Auth = () =>{
                             value={password}
                         />
                     </div>
-                    <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                    <button onClick={ variant==='login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                     {variant === 'login' ? 'Login' : 'Sign up'}
                     </button>
                     <p className="text-neutral-500 mt-12">
