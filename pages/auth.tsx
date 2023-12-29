79fa36f60a1db3e969ca16c780becc7d0b09aba1
Dev <devbajaj.bits@gmail.com>
2023-12-29 19:32:11 +0530

    profiles screen

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,14 +2,14 @@ import Input from "@/components/Input";
 import {useCallback, useState} from 'react';
 import axios from "axios";
 import { signIn } from 'next-auth/react';
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 import { sign } from "crypto";
 
 const Auth = () =>{
-    const router = useRouter();
+    // const router = useRouter();
 
     const [email,setEmail]=useState('');
     const [name, setName] = useState('');
@@ -26,15 +26,15 @@ const Auth = () =>{
             await signIn('credentials',{
                 email,
                 password,
-                redirect: false,
-                callbackUrl: '/'
+                // redirect: false,
+                callbackUrl: '/profiles'
             });
-            router.push('/');
+            // router.push('/profiles');
             console.log("loginn")
         } catch (error) {
             console.log(error);
         }
-    },[email, password,router]);
+    },[email, password]);
 
     const register = useCallback(async () => {
         try {
@@ -93,12 +93,12 @@ const Auth = () =>{
                     </button>
                     <div className="flex flex-row items-center gap-4 mt-8 justify-center">
                         <div 
-                        onClick={()=>signIn('google',{callbackUrl: '/'})}
+                        onClick={()=>signIn('google',{callbackUrl: '/profiles'})}
                         className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                             <FcGoogle size={30} />
                         </div>
                         <div
-                        onClick={()=>signIn('github',{callbackUrl: '/'})}
+                        onClick={()=>signIn('github',{callbackUrl: '/profiles'})}
                         className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                             <FaGithub size={30} />
                         </div>
